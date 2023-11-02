bec24d449dc55fe833f9ca691eacf15aa6b91c92
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-11-02 16:11:09 -0300

    refactor navbar and home

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,73 +1,37 @@
 const Navbar = () => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-[#242424]">
-      <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-          {/* <img
-            src=""
-            className="h-8 mr-3"
-            alt="Silveira"
-          /> */}
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            fsilveira
-          </span>
-        </a>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-[#242424] dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-              >
-                home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-              >
-                posts
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                sobre mim
-              </a>
-            </li>
-          </ul>
+    <header className="fixed z-40 flex flex-row items-center justify-between w-full h-16 p-5 transition-all duration-300 ease-in-out shadow-lg lg:px-10 bg-primary/50 backdrop-blur-lg">
+      <nav className="flex items-center justify-between w-full">
+        <div className="transition-colors duration-200 group hover:text-link">
+          <a href="/">
+            <div className="relative w-12 h-12">
+              {/* <img
+                alt="logo image"
+                decoding="async"
+                data-nimg="fill"
+                className="object-cover object-center transition-all duration-300 ease-in-out filter group-hover:opacity-50"
+                src="/img/logo.svg"
+              /> */} Felipe Silveira
+            </div>
+          </a>
         </div>
-      </div>
-    </nav>
+        <ul className="justify-between hidden gap-6 lg:flex">
+          <li className="relative font-light transition-colors hover:text-link text-md false">
+            <a href="/">Home</a>
+          </li>
+          <li className="relative font-light transition-colors hover:text-link text-md false">
+            <a href="/">Posts</a>
+          </li>
+          <li className="relative font-light transition-colors hover:text-link text-md false">
+            <a href="/">Sobre mim</a>
+          </li>
+          <li className="relative font-light transition-colors hover:text-link text-md false">
+            <a href="/">Contato</a>
+          </li>
+        </ul>
+        <div className="z-50 flex transition-all duration-200 hover:text-link lg:hidden"></div>
+      </nav>
+    </header>
   );
 };
 
