c1da92216dae2f8d31e540d09ea83c904807aa29
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-11-02 13:40:14 -0300

    refactor footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,20 +3,76 @@ import dayjs from "dayjs";
 
 const Footer = () => {
   return (
-    <footer className="p-4">
-      <div className="flex justify-center gap-4">
-        <a href="https://www.linkedin.com/in/felipesilveirasp/" target="_blank" rel="noreferrer">
-          <LinkedinLogo className="w-8 h-8" />
-        </a>
-        <a href="https://github.com/felipe-silveira-dev" target="_blank" rel="noreferrer">
-            <GithubLogo className="w-8 h-8" />
-        </a>
-      </div>
-      <div className="mt-4 border-t border-white/10 pt-8 sm:mt-6 lg:mt-4">
-          <p className="text-xs leading-5 text-gray-400">
-            &copy; {dayjs().year()} Felipe Silveira, Inc. All rights reserved.
-          </p>
+    <footer>
+      <div className="w-full max-w-4xl px-6 mx-auto lg:max-w-7xl">
+        <div className="flex flex-col pt-6 pb-4 border-t border-white/10">
+          <div className="flex justify-between w-full pb-10">
+            <div className="flex flex-col gap-6 max-w-7xl sm:max-w-xs">
+              <div className="transition-colors duration-200 group hover:text-link">
+                <a
+                  href="https://www.linkedin.com/in/felipesilveirasp/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Felipe Silveira
+                </a>
+              </div>
+              <p>Junior Software Engineer</p>
+            </div>
+            <div className="flex flex-col gap-6 max-w-7xl sm:max-w-xs">
+              <p className="text-xl font-bold">Redes</p>
+              <ul className="flex flex-col justify-between gap-4">
+                <li className="relative font-light transition-colors hover:text-link text-md undefined">
+                  <a
+                    className="flex items-center"
+                    href="https://www.linkedin.com/in/felipesilveirasp/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <LinkedinLogo className="w-8 h-8" />{" "}
+                    <p className="ml-1">Linkedin</p>
+                  </a>
+                </li>
+                <li className="relative font-light transition-colors hover:text-link text-md undefined">
+                  <a
+                    className="flex items-center"
+                    href="https://github.com/felipe-silveira-dev"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <GithubLogo className="w-8 h-8" />{" "}
+                    <p className="ml-1">Github</p>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col gap-6 max-w-7xl sm:max-w-xs">
+              <p className="text-xl font-bold">Sitemap</p>
+              <nav className="flex flex-col gap-4 text-left">
+                <ul className="flex flex-col justify-between gap-4">
+                  <li className="relative font-light transition-colors hover:text-link text-md false">
+                    <a href="/">Home</a>
+                  </li>
+                  <li className="relative font-light transition-colors hover:text-link text-md false">
+                    <a href="/posts">Posts</a>
+                  </li>
+                  <li className="relative font-light transition-colors hover:text-link text-md false">
+                    <a href="/about">Sobre mim</a>
+                  </li>
+                  <li className="relative font-light transition-colors hover:text-link text-md false">
+                    <a href="/contact">Contato</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="w-full max-w-4xl mx-auto border-t border-white/10 sm:mt-6 lg:mt-4 lg:max-w-7xl">
+        <p className="mt-4 text-xs leading-5 text-gray-400">
+          &copy; {dayjs().year()} Felipe Silveira, Inc. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,70 @@
-import React from "react";
-
 const Navbar = () => {
   return (
-    <nav className="">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <div></div>
-        <div>
-            nav
+    <nav className="bg-white border-gray-200 dark:bg-[#242424]">
+      <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
+        <a href="https://flowbite.com/" className="flex items-center">
+          {/* <img
+            src=""
+            className="h-8 mr-3"
+            alt="Silveira"
+          /> */}
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            fsilveira
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-[#242424] dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                posts
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                sobre mim
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
